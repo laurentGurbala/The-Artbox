f3d74db281967592a887e2af968227b9f7aba6a0
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2024-11-06 14:42:59 +0100

    Récupération des données via la base de donnée

---
+++ data/bdd.php
@@ -0,0 +1,10 @@
+<?php
+
+function connexion()
+{
+    return new PDO(
+        "mysql:host=localhost;dbname=artbox;charset=utf8",
+        "root",
+        ""
+    );
+}
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php include "templates/header.php"; ?>
-<?php include "data/oeuvres.php"; ?>
+<?php require_once "data/bdd.php";
+
+$bdd = connexion();
+
+$oeuvresStatement = $bdd->prepare("SELECT * FROM oeuvres");
+$oeuvresStatement->execute();
+$oeuvres = $oeuvresStatement->fetchAll();
+?>
+
 
 <div id="liste-oeuvres">
     <?php foreach ($oeuvres as $oeuvre): ?>
@@ -7,7 +15,7 @@
             <a href="oeuvre.php?id=<?php echo $oeuvre['id']; ?>">
                 <img src="<?php echo $oeuvre['image']; ?>" alt="<?php echo $oeuvre['title']; ?>">
                 <h2><?php echo $oeuvre['title']; ?></h2>
-                <p class="description"><?php echo $oeuvre['artist']; ?></p>
+                <p class="description"><?php echo $oeuvre['artiste']; ?></p>
             </a>
         </article>
     <?php endforeach; ?>
